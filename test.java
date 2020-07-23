9e5242c8eb719ebf53a035592eb451dae9172765
Bharatgupta922 <gbharat922@gmail.com>
2020-07-23 16:35:48 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,12 @@
+class test{
+
+    public static void main (String[] args){
+boolean b ;
+
+int sum = 0 ; 
+for(int i = 0 , j = 0 ; i < 5 & j<5 ; ++i , j = i+1){
+    sum+=i;
+}
+System.out.println(sum);;
+    }
+}
